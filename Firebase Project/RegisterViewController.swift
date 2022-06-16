98502525e2c0a2b319adaf75290709acf1ace8ab
eminemini <eminrmae@gmail.com>
2022-06-16 17:08:20 +0200

    Implement Registration

--- Firebase Project/RegisterViewController.swift
+++ Firebase Project/RegisterViewController.swift
@@ -6,6 +6,8 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class RegisterViewController: UIViewController {
     
@@ -21,10 +23,45 @@ class RegisterViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func registerTouchUp(_ sender: Any) {
+        registerFlow()
     }
     
     @IBAction func loginTouchUp(_ sender: Any) {
         self.dismiss(animated: true)
     }
     
+}
+
+// MARK: - Firebase Authentication
+extension RegisterViewController {
+    func signUpAuth(_ email: String, _ password: String) {
+        Auth.auth().createUser(withEmail: email, password: password) { result, error in
+            guard let user = result?.user, error == nil else {
+                print("Failed to created user.\nError:\(error?.localizedDescription)")
+                return
+            }
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "feedVC")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
+}
+
+// MARK: - TextField Validation
+extension RegisterViewController {
+    func registerFlow() {
+        guard let email = emailTextField.text else {
+            print("Email cannot be empty")
+            return
+        }
+        
+        guard let password = passwordTextField.text else {
+            print("Password cannot be empty")
+            return
+        }
+        
+        signUpAuth(email, password)
+    }
 }

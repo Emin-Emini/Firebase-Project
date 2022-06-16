e95d49c1bd16a868f365ec19e1f75e5b22657cba
eminemini <eminrmae@gmail.com>
2022-06-16 17:14:45 +0200

    Implement Login

--- Firebase Project/LoginViewController.swift
+++ Firebase Project/LoginViewController.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import Firebase
 
 class LoginViewController: UIViewController {
 
@@ -21,6 +22,7 @@ class LoginViewController: UIViewController {
     
     // MARK: - Actions
     @IBAction func loginTouchUp(_ sender: Any) {
+        loginFlow()
     }
     
     @IBAction func registerTouchUp(_ sender: Any) {
@@ -28,3 +30,36 @@ class LoginViewController: UIViewController {
     
 }
 
+// MARK: - Firebase Authentication
+extension LoginViewController {
+    func loginAuth(_ email: String, _ password: String) {
+        Auth.auth().signIn(withEmail: email, password: password) { result, error in
+            guard let user = result?.user, error == nil else {
+                print("Failed to login user.\nError:\(error?.localizedDescription)")
+                return
+            }
+            
+            let storyboard = UIStoryboard(name: "Main", bundle: nil)
+            let vc = storyboard.instantiateViewController(withIdentifier: "feedVC")
+            vc.modalPresentationStyle = .overFullScreen
+            self.present(vc, animated: true)
+        }
+    }
+}
+
+// MARK: - TextField Validation
+extension LoginViewController {
+    func loginFlow() {
+        guard let email = emailTextField.text else {
+            print("Email cannot be empty")
+            return
+        }
+        
+        guard let password = passwordTextField.text else {
+            print("Password cannot be empty")
+            return
+        }
+        
+        loginAuth(email, password)
+    }
+}
